c817e9cce69ec01a03802f0238f7000f746c2b4f
Peter Abbondanzo <peter@abbondanzo.com>
2024-06-26 14:52:19 -0400

    Fix formatting issues and accept output directory

--- src/main/java/com/vector/svg2vectorandroid/Runner.java
+++ src/main/java/com/vector/svg2vectorandroid/Runner.java
@@ -5,17 +5,20 @@ package com.vector.svg2vectorandroid;
  */
 public class Runner {
 
-    public static void main(String args[]){
-
-        if(args.length == 0){
-            System.out.println(" Provide source directory as first arguement for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Provide source directory as first argument for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
             return;
         }
 
         String sourceDirectory = args[0];
-        if(null != sourceDirectory && !sourceDirectory.isEmpty()){
-            SvgFilesProcessor processor = new SvgFilesProcessor(sourceDirectory);
-            processor.process();
+        SvgFilesProcessor processor;
+        if (args.length == 1) {
+            processor = new SvgFilesProcessor(sourceDirectory);
+        } else {
+            String destinationDirectory = args[1];
+            processor = new SvgFilesProcessor(sourceDirectory, destinationDirectory);
         }
+        processor.process();
     }
 }
